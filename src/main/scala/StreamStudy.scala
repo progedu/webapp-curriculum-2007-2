d354cf1acbdf86d5073d66e421a1d784ce462e04
Masahiro <masahiro.infinite77@gmail.com>
2018-12-15 06:58:22 +0900

    Streamの実装

--- src/main/scala/StreamStudy.scala
+++ src/main/scala/StreamStudy.scala
@@ -1,9 +1,18 @@
 trait StreamStudy[+A] {
 
-  def headOption: Option[A] = ???
+  def headOption: Option[A] = {
+    this match {
+      case EmptyStream => None
+      case a: Cons[A] => Option(a.h())
+    }
+  }
 
-  def tail: StreamStudy[A] = ???
-
+  def tail: StreamStudy[A] = {
+    this match {
+      case EmptyStream => EmptyStream
+      case a: Cons[A] => a.t()
+    }
+  }
 }
 
 case object EmptyStream extends StreamStudy[Nothing]
@@ -12,7 +21,9 @@ case class Cons[+A](h: () => A, t: () => StreamStudy[A]) extends StreamStudy[A]
 
 object StreamStudy {
 
-  def cons[A](h: => A, t: => StreamStudy[A]): StreamStudy[A] = ???
+  def cons[A](h: => A, t: => StreamStudy[A]): StreamStudy[A] = {
+    Cons(() => h, () => t)
+  }
 
   def empty[A]: StreamStudy[A] = EmptyStream
 
